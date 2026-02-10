3c0c9c04a326e12c0ffb71a63051fbc68a5be89a
Arshiapat <patel.arshia22@gmail.com>
2026-02-10 17:05:18 -0500

    Added POST endpoint for inhaler usage

--- breathing_bud/backend/server.js
+++ breathing_bud/backend/server.js
@@ -18,21 +18,44 @@ const pool = new Pool({
 });
 
 //Initializes the database
+// Table for inhaler usage
+// columnName RULES
 pool.query(`
-  CREATE TABLE IF NOT EXISTS items (
+  CREATE TABLE IF NOT EXISTS inhaler_usage (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    user_id INT NOT NULL,
+    inhaler_type VARCHAR(100) NOT NULL,
+    dose INT NOT NULL,
+    taken_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   )
-`).then(() => console.log('Database initialized'));
+`).then(() => console.log('Inhaler usage db table initialized'));
 
 //Routes
 app.get('/api/health', (req, res) => {
   res.json({ message: 'Backend is running!' });
 });
 
-app.get('/api/items', async (req, res) => {
-  const result = await pool.query('SELECT * FROM items');
-  res.json(result.rows);
+// Route for inhaler usage
+// Creates POST endpoint, which means frontend is sending data to server
+app.get('/api/inhaler', async (req, res) => {
+  try {
+    // Takes data send by frontend (expect JSON)
+    // These three values will be saved to db
+    const {user_id, inhaler_type, dose} = req.body;
+    // INSET INTO adds new row to inhaler_usage table
+    // $1, $2, $3 are placeholders for the values in the array [user_id, inhaler_type, dose]
+    // RETURNING * means that after inserting the new row, the database will return the entire row that was just inserted to frontend
+    const result = await pool.query('INSERT INTO inhaler_usage (user_id, inhaler_type, dose) VALUES ($1, $2, $3) RETURNING *',
+      [user_id, inhaler_type, dose]);
+    
+    // Sends new row back to frontend
+    res.json(result.rows[0]);
+  // Catches error and sends error message back to frontend
+  } catch (error) {
+    console.error('Error saving inhaler usage:', error);
+    res.status(500).json({ error: 'Failed to save inhaler usage' });
+  }
+  
 });
 
 app.post('/api/items', async (req, res) => {
